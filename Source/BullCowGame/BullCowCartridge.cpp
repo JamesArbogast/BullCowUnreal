8d7ed8497458556bd072039c04e0d54142f2165e
James Arbogast <jamesarbogast52@gmail.com>
2021-10-07 12:24:11 -0700

    win lose print

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,22 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Moo there!"));
+    PrintLine(TEXT("Guess the 4 letter word!")); //magic number remove
+    PrintLine(TEXT("Press enter to continue..."));
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString& Input) // when the player hits enter
 {
-    
+    ClearScreen();
+    FString HiddenWord = TEXT("soup"); //move outside function
+
+    if(Input == HiddenWord)
+    {
+        PrintLine(TEXT("Moo Win!"));
+    }
+    else
+    {
+        PrintLine(TEXT("Moo Lose!"));
+    }
 }
